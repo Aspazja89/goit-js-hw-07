cf538dac582b7e00736334650ff936dc95553608
Aspazja89 <144452780+Aspazja89@users.noreply.github.com>
2024-04-17 12:06:37 +0200

    e

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// Znajdź kontener galerii
+const galleryContainer = document.querySelector(".gallery");
 
+// Funkcja renderująca elementy galerii
+const renderGallery = (items, container) => {
+  const galleryMarkup = items
+    .map(({ original, preview, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+
+  container.innerHTML = galleryMarkup;
+};
+
+// Renderuj galerię
+renderGallery(galleryItems, galleryContainer);
+
+// Nasłuchuj kliknięć na obrazkach w galerii
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault(); // Zapobiegnij domyślnej akcji przekierowania
+
+  // Sprawdź czy kliknięto na obrazek
+  if (event.target.nodeName === "IMG") {
+    // Pobierz adres obrazka docelowego
+    const largeImageURL = event.target.dataset.source;
+
+    // Utwórz nowy obiekt basicLightbox z adresem obrazka docelowego
+    const instance = basicLightbox.create(`
+      <img src="${largeImageURL}" alt="">
+    `);
+
+    // Otwórz okno modalne
+    instance.show();
+  }
+});
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// Znajdź kontener galerii
+const galleryContainer = document.querySelector(".gallery");
 
+// Funkcja renderująca elementy galerii
+const renderGallery = (items, container) => {
+  const galleryMarkup = items
+    .map(({ original, preview, description }) => {
+      return `
+        <li>
+          <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+
+  container.innerHTML = galleryMarkup;
+};
+
+// Renderuj galerię
+renderGallery(galleryItems, galleryContainer);
+
+// Inicjalizacja SimpleLightbox po załadowaniu zawartości strony
+document.addEventListener("DOMContentLoaded", () => {
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250, // Opóźnienie wyświetlania podpisu
+  });
+});
 console.log(galleryItems);
